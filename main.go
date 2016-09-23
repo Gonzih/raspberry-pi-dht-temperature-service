df62e39dd33842902be17415d8b82e9a7d927d9f
Max Gonzih <gonzih@gmail.com>
2016-09-23 11:15:12 +0200

    No need for iris framework in here i think

--- main.go
+++ main.go
@@ -1,29 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"sync"
 
 	"github.com/d2r2/go-dht"
-	"github.com/kataras/iris"
+	"github.com/julienschmidt/httprouter"
 )
 
 var gpioMutex sync.Mutex
 
-func temperatureHandler(ctx *iris.Context) {
+func temperatureHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	gpioMutex.Lock()
 	defer gpioMutex.Unlock()
 
 	temperature, humidity, retried, err := dht.ReadDHTxxWithRetry(dht.DHT11, 4, true, 10)
 
 	if err != nil {
-		ctx.Write("Error occured during temp readout: %s", err)
+		fmt.Fprintf(w, "Error occured during temp readout: %s", err)
 	} else {
-		ctx.Write("Temperature = %v*C, Humidity = %v%% (retried %d times)", temperature, humidity, retried)
+		fmt.Fprintf(w, "Temperature = %v*C, Humidity = %v%% (retried %d times)", temperature, humidity, retried)
 	}
 }
 
 func main() {
-	iris.Get("/", temperatureHandler)
+	router := httprouter.New()
+	router.GET("/", temperatureHandler)
 
-	iris.Listen(":8080")
+	log.Println("Ready to serve!")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
